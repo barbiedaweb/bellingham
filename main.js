327987307e30fe6517a4d67818e9b3809e81186c
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-10-08 08:12:53 -0300

    aaa

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-function criaCartao (categoria, pergunta, resposta){
+function criaCartao(categoria,pergunta,resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML = `
-    <div class="cartao-conteudo">
-    <h3>${categoria}</h3>
+    cartao.innerHTML = `<div class="cartao-conteudo">
+    <h3> ${categoria} </h3>
+    
     <div class="cartao-pergunta">
         <p>${pergunta}</p>
-    </div>
-    <div class="cartao-resposta"></div>
-        <p>${resposta}</p>
-</div>`
+        </div>
 
-let respostaVisivel = false;
-function viraCartao(){
-    respostaVisivel =!respostaVisivel;
-    cartao.classList.toggle('active', respostaVisivel);
-}
-    cartao.addEventListener ('click', viraCartao);
-    container.appendChild(cartao);
+        <div class="cartao-resposta">
+        <p>${resposta}</p>
+        </div>
+        </div>
+        `
+        let respostaVisivel = false;
+        function viraCartao(){
+            respostaVisivel =!respostaVisivel;
+            cartao.classList.toggle('active', respostaVisivel);
+        }
+cartao.addEventListener ('click', viraCartao);
+        container.appendChild(cartao);
+
 }
